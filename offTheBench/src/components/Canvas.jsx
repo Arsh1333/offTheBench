76ce80dab31ea70be447c7a2ad73583d938b776d
Arsh <pawar.arsh1333@gmail.com>
2025-08-12 16:01:43 +0530

    basic canvas

--- offTheBench/src/components/Canvas.jsx
+++ offTheBench/src/components/Canvas.jsx
@@ -1,7 +1,38 @@
+import { Stage, Layer, Circle } from "react-konva";
+import { useState } from "react";
 function Canvas() {
+  const [position, setPosition] = useState({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  });
   return (
     <>
       <h1>Canvas</h1>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Circle
+            x={position.x}
+            y={position.y}
+            radius={70}
+            fill="red"
+            stroke="black"
+            strokeWidth={4}
+            draggable
+            onMouseEnter={(e) => {
+              document.body.style.cursor = "pointer";
+            }}
+            onMouseLeave={(e) => {
+              document.body.style.cursor = "default";
+            }}
+            onDragEnd={(e) => {
+              setPosition({
+                x: e.target.x(),
+                y: e.target.y(),
+              });
+            }}
+          />
+        </Layer>
+      </Stage>
     </>
   );
 }
